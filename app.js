d3b2c4ccef764b077b5b0dfe49c70e6a8c7bf49b
LivinkumarS <001liv100@ggmail.com>
2025-05-29 12:51:20 +0530

    DataTypes--> Premitive DataType

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
-console.log("I like you");
+// const a = 10;
+// a = 20;
+// console.log(a);
+
+// Number
+// let a=10;
+// let b=10.78378;
+// console.log(b);
+// let str = "63483487hifgevfu";
+// let num = parseInt(str);
+// let str = "3.14hbsguug";
+// let num = parseFloat(str);
+// console.log(num);
+// console.log(typeof num);
+// let a = 8.4555644;
+// console.log(a.toFixed(5));
+// let str = a.toString();
+// console.log(str);
+// console.log(typeof str);
+
+// String
+
+// let a = "I Would Like to Code";
+// let b = 'I Would Like to Code';
+// let c = `I Would Like to Code`;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+let str = "Hello world";
+// console.log(str.length);
+let cap = str.toUpperCase();
+let low = str.toLowerCase();
+// console.log(cap);
+// console.log(low);
+// console.log(str.includes("o w"));
+// console.log(str.charAt(4));
+
+console.log(cap.slice(1,9));
+
+
+// Boolean
+
+// let a=true
+// let b=false
+
+// console.log(a);
+// console.log(b);
+
+// undefined
+
+// let a;
+// console.log(a);
+
+// null
+// let a = null;
+// console.log(a);
