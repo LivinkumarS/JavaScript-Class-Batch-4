ec208971b26fb6e28ee627a23ebf708ab3fe41a8
LivinkumarS <001liv100@ggmail.com>
2025-06-02 13:40:37 +0530

    Logical Operator and Conditional Clauses

--- app.js
+++ app.js
@@ -1,20 +1,48 @@
-// Assignment operators
+// AND
+// let a= false;
+// let b= false;
+// console.log(a && b);
 // let a = 10;
-// a += 20; //a=30
-// a -= 10; //a=20
-// a *= 2; //a=40
-// a /= 4; //a=10
-// console.log(a);
+// let b = 12;
+// console.log(a < b && a >= b);
 
-// Comparison Operators
+// OR
 
-let a = -10;
-let b = 9;
+// let a = false;
+// let b = false;
+// console.log(a || b);
 
-// console.log(a != b);
-// console.log(++a > b);
-// console.log(a < b);
+// NOT
+// let a = false;
+// console.log(!a);
 
+// let a = 10;
+// let b = 11;
+// let c = ((a * b ** 2) / 7) % 3;
 
-// console.log(a >= b);
-console.log(a <= b);
+// console.log(a < b || (b > a && a >= b));
+// console.log((c * a > b * c || (a * b < c && !(a > b || b > c))) && b < a);
+
+// Conditional Clauses
+
+// let a = 10;
+// let b = 100;
+
+// if (b != a ** 2) {
+//   console.log("Hello World");
+// } else if (a > b) {
+//   console.log("Coming to World");
+// } else if (a === b) {
+//   console.log("Going Out from world");
+// } else {
+//   console.log("Good bye world");
+// }
+
+let number = prompt("Type number");
+
+if (parseInt(number) % 2 == 0) {
+  console.log("Even Number");
+} else {
+  console.log("Odd number");
+}
+
