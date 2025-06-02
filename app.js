008edd1afe49ad087845df94c7e90ff6ba3b3fd7
LivinkumarS <001liv100@ggmail.com>
2025-06-02 13:44:06 +0530

    Task Question(If, else)

--- app.js
+++ app.js
@@ -1,48 +1,10 @@
-// AND
-// let a= false;
-// let b= false;
-// console.log(a && b);
-// let a = 10;
-// let b = 12;
-// console.log(a < b && a >= b);
+// Conditional Clauses Task 
 
-// OR
+// prompt mark 
 
-// let a = false;
-// let b = false;
-// console.log(a || b);
-
-// NOT
-// let a = false;
-// console.log(!a);
-
-// let a = 10;
-// let b = 11;
-// let c = ((a * b ** 2) / 7) % 3;
-
-// console.log(a < b || (b > a && a >= b));
-// console.log((c * a > b * c || (a * b < c && !(a > b || b > c))) && b < a);
-
-// Conditional Clauses
-
-// let a = 10;
-// let b = 100;
-
-// if (b != a ** 2) {
-//   console.log("Hello World");
-// } else if (a > b) {
-//   console.log("Coming to World");
-// } else if (a === b) {
-//   console.log("Going Out from world");
-// } else {
-//   console.log("Good bye world");
-// }
-
-let number = prompt("Type number");
-
-if (parseInt(number) % 2 == 0) {
-  console.log("Even Number");
-} else {
-  console.log("Odd number");
-}
-
+// 91,92..100--> console.log("A+");
+// 81,82...90 --> A
+// 71-80 ---> B+
+// 61-70 ---> B
+// 50-60 ---> C
+// <50 --> Fail
