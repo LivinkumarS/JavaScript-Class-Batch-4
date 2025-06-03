60d6988b0d945a782453f98fd3e0437b71fd3c46
LivinkumarS <001liv100@ggmail.com>
2025-06-03 13:44:22 +0530

    Loops in Js (for, while and do while)

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-let a = 4;
-// if (a > 5) {
-//   console.log("Greater");
-// } else {
-//   console.log("Lesser");
+// for (let i = 0; i < 10; i++) {
+//   console.log("Step " + i);
 // }
 
-a > 5 ? console.log("Greater") : console.log("Lesser");
+//i=0,-1,-2,-3...............
+
+// iter 1
+// iter 2
+// iter 3
+
+// iter 10
+
+// While loop
+
+// let goLoop=false;
+// let secret=66;
+
+// while (goLoop) {
+//   let inpNum=prompt("Enter Number")
+//   if(parseInt(inpNum)===secret){
+//     goLoop=false;
+//     console.log("You Got the answer!");
+//   }
+// }
+
+// iter 1->10,11
+
+// 33
+
+//i=0,1...9
+
+//iter 1,...10
+
+// Do while  loop
+
+let i = 10;
+
+console.log("From Do while:");
+
+do {
+    console.log(i);
+    i++;
+} while (i < 10);
+
+console.log("From while:");
+
+let a=10;
+
+while (a < 10) {
+  console.log(a);
+  a++;
+}
