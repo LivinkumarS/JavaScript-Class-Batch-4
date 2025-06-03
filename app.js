b5624a81990dab356ec39b828c1b6d98e7017c10
LivinkumarS <001liv100@ggmail.com>
2025-06-03 13:51:04 +0530

    Truthy and falsy values

--- app.js
+++ app.js
@@ -1,52 +1,6 @@
-// for (let i = 0; i < 10; i++) {
-//   console.log("Step " + i);
-// }
+let a = -100;
+let b;
 
-//i=0,-1,-2,-3...............
-
-// iter 1
-// iter 2
-// iter 3
-
-// iter 10
-
-// While loop
-
-// let goLoop=false;
-// let secret=66;
-
-// while (goLoop) {
-//   let inpNum=prompt("Enter Number")
-//   if(parseInt(inpNum)===secret){
-//     goLoop=false;
-//     console.log("You Got the answer!");
-//   }
-// }
-
-// iter 1->10,11
-
-// 33
-
-//i=0,1...9
-
-//iter 1,...10
-
-// Do while  loop
-
-let i = 10;
-
-console.log("From Do while:");
-
-do {
-    console.log(i);
-    i++;
-} while (i < 10);
-
-console.log("From while:");
-
-let a=10;
-
-while (a < 10) {
-  console.log(a);
-  a++;
+if (b) {
+  console.log("Hello World");
 }
