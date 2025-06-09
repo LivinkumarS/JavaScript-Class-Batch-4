15dd0212924547561f554ceb64402b8b07fce91a
LivinkumarS <001liv100@ggmail.com>
2025-06-09 14:15:18 +0530

    Rest, Spread and Destructuring

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, "the", "apple"];
+// Destructuring
+
+// let fnum = arr[0];
+// let mnum = arr[1];
+// let lnum = arr[2];
+
+// let [fnum, mnum, lnum] = arr;
+
+// console.log(lnum);
+
+// let { fName, lNAme, list } = obj;
+
+// console.log(list);
+
+let arr = [243, 234, 35, 34, 3, 2134, 46, 234, 678];
+
+let arr1 = [1, ...arr, 2, 3];
+
+// arr1 = arr1.concat(arr);
+// console.log(arr1);
 
 let obj = {
-  fName: "Livin",
-  lName: "kumar",
-  age: 88,
-  canVote: true,
-  printName() {
-    console.log(
-      `Hi my name is ${this.fName}${this.lName}. I am ${this.age} years old!`
-    );
-  },
-  list: [1, 2, 3, 4, 5],
+  fName: "saala",
+  lNAme: "kumar",
+  list: [23, 34, 32, 35],
 };
 
-for (let i in obj) {
-  console.log(obj[i]);
-}
+let obj1 = {
+  ...obj,
+  id: "1652375174",
+};
+
+// console.log([...arr, ...obj1.list]);
+
+let { list, ...rest } = obj;
+console.log(rest);
+console.log(list);
