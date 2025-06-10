23a9620e70c10d50a8102a2bf57269897acdae1c
LivinkumarS <001liv100@ggmail.com>
2025-06-10 13:22:30 +0530

    NaN

--- app.js
+++ app.js
@@ -1,37 +1,14 @@
-// Destructuring
+// let number = parseInt("hello");
+// // console.log(number);
 
-// let fnum = arr[0];
-// let mnum = arr[1];
-// let lnum = arr[2];
+// console.log(0 / 0);
 
-// let [fnum, mnum, lnum] = arr;
+let a;
+let b = null;
+let c = 0 / 0;
 
-// console.log(lnum);
+// console.log(c);
+// console.log(isNaN(c));
 
-// let { fName, lNAme, list } = obj;
 
-// console.log(list);
-
-let arr = [243, 234, 35, 34, 3, 2134, 46, 234, 678];
-
-let arr1 = [1, ...arr, 2, 3];
-
-// arr1 = arr1.concat(arr);
-// console.log(arr1);
-
-let obj = {
-  fName: "saala",
-  lNAme: "kumar",
-  list: [23, 34, 32, 35],
-};
-
-let obj1 = {
-  ...obj,
-  id: "1652375174",
-};
-
-// console.log([...arr, ...obj1.list]);
-
-let { list, ...rest } = obj;
-console.log(rest);
-console.log(list);
+console.log(typeof c);
