e6d855076a852256dceff776602f3ed6408aefe3
LivinkumarS <001liv100@ggmail.com>
2025-06-10 13:44:50 +0530

    DOM Basics ans Selecting Tags

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// let number = parseInt("hello");
-// // console.log(number);
-
-// console.log(0 / 0);
-
-let a;
-let b = null;
-let c = 0 / 0;
-
-// console.log(c);
-// console.log(isNaN(c));
+// let h1 = document.getElementById("head");
+// let h1 = document.getElementsByClassName("head");
+// let h1 = document.getElementsByTagName("h1");
 
 
-console.log(typeof c);
+// let h1 =document.querySelector("h2")
+let h1 =document.querySelectorAll("h1")
+console.log(h1);
