4ae37b6d63f2c93372ff2520ad66842dd2b002b2
LivinkumarS <001liv100@ggmail.com>
2025-06-12 14:00:09 +0530

    Task Update

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-const btn = document.getElementsByTagName("button");
-const ul = document.querySelector("ul");
-const inp = document.querySelector("input");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
 
-function printHi(e) {
-  console.log(e.target);
-}
-
-// inp.addEventListener("click",printHi)
-
-// Array.from(btn).forEach((item, ind) => {
-//   item.addEventListener("click", printHi);
-// });
-
-// for (let i = 0; i < btn.length; i++) {
-//   btn[i].addEventListener("click", (event) => {
-//     console.log(event.target);
-//   });
-// }
+document.getElementById("add").addEventListener("click", () => {
+  let theWord = inp.value;
+  inp.value = "";
+  if (theWord === "") {
+    return;
+  }
+  let newLi = document.createElement("li");
+  newLi.innerHTML = `${theWord} <button>del</button>`;
+  ul.appendChild(newLi);
+});
