fb5beb80c7350271e65207879e31388537cec85c
LivinkumarS <001liv100@ggmail.com>
2025-06-13 13:47:19 +0530

    Task Finished

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ document.getElementById("add").addEventListener("click", () => {
   let newLi = document.createElement("li");
   newLi.innerHTML = `${theWord} <button>del</button>`;
   ul.appendChild(newLi);
+
+  document.querySelector("button");
+
+  newLi.querySelector("button").addEventListener("click", () => {
+    newLi.remove();
+  });
 });
+
+// let ul=document.querySelector("ul")
+
+// console.log(ul.querySelector("button"));
