b29163a9207a8919a2a7a4b9c9078623927303e4
LivinkumarS <001liv100@ggmail.com>
2025-07-09 13:43:08 +0530

    useState and Conditional Rendering

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import Card from "./Card.jsx";
-import vjimg from "./assets/actors-imgs/vijay.jpg";
-import surImg from "./assets/actors-imgs/suyra.jpg";
-import dImg from "./assets/actors-imgs/dhanush.jpg";
 
 function App() {
   return (
     <div>
       <h1 className="title">Actors</h1>
-      <Card img={vjimg} name="Vijay" age="50" movies="68" />
-      <Card img={surImg} name="Surya" age="48" movies="50" />
-      <Card img={dImg} name="Dhanush" age="45" movies="40" />
+      <Card/>
     </div>
   );
 }
--- react-project/src/Card.jsx
+++ react-project/src/Card.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 
+export default function Card() {
+  const [number, setNumber] = useState(1);
+  const [condition, setCondition] = useState(false);
 
-export default function Card(prop) {
+  console.log(`Condition: ${condition}`);
+
+  // prev+1---->new
 
   return (
     <div className="card">
-      <img src={prop.img} alt="actor image" />
-      <div className="text">
-        <h2>Name: {prop.name}</h2>
-        <p>Age: {prop.age}</p>
-        <p>No Of Movies Acted: {prop.movies}</p>
-      </div>
+      <h1>Number: {number}</h1>
+      <button
+        onClick={() => {
+          setNumber((prev) => {
+            return prev + 1;
+          });
+        }}
+      >
+        click
+      </button>
+      <br />
+
+      {condition ? <h1>Hidden Element1</h1> : <h1>Hidden Element2</h1>}
+
+      <button
+        onClick={() => {
+          setCondition((prev) => {
+            return !prev;
+          });
+        }}
+      >
+        click
+      </button>
     </div>
   );
 }
