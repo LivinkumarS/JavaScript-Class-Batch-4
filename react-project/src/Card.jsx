5648d2c31adcd09b5068c5667cfb82cfcbc21c3b
LivinkumarS <001liv100@ggmail.com>
2025-07-09 13:59:20 +0530

    task update

--- react-project/src/Card.jsx
+++ react-project/src/Card.jsx
@@ -2,37 +2,29 @@ import { useState } from "react";
 
 export default function Card() {
   const [number, setNumber] = useState(1);
-  const [condition, setCondition] = useState(false);
-
-  console.log(`Condition: ${condition}`);
 
   // prev+1---->new
+
+  function add() {
+    setNumber((prev) => {
+      return prev + 1;
+    });
+  }
+  function reduce() {
+    setNumber((prev) => {
+      if (prev == 1) {
+        return prev;
+      }
+      return prev - 1;
+    });
+  }
 
   return (
     <div className="card">
       <h1>Number: {number}</h1>
-      <button
-        onClick={() => {
-          setNumber((prev) => {
-            return prev + 1;
-          });
-        }}
-      >
-        click
-      </button>
-      <br />
-
-      {condition ? <h1>Hidden Element1</h1> : <h1>Hidden Element2</h1>}
-
-      <button
-        onClick={() => {
-          setCondition((prev) => {
-            return !prev;
-          });
-        }}
-      >
-        click
-      </button>
+      <button onClick={add}>Add Button</button> <br />
+      <button onClick={reduce}>Reduce Button</button>
+      <h2>{number % 2 == 0 ? "Even number" : "Odd number"}</h2>
     </div>
   );
 }
